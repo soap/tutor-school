fa28fc4825beabb3fd98239bdd2d3e4606f475e8
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2016-06-04 22:28:39 +0700

    Add login, register page to acceptant test.

--- tests/acceptance/AllPagesCept.php
+++ tests/acceptance/AllPagesCept.php
@@ -1,5 +1,13 @@
-<?php 
+<?php
 $I = new AcceptanceTester($scenario);
 $I->wantTo('Ensure that frontpage works');
 $I->amOnPage('/');
 $I->see('Home');
+
+$I->wantTo('Ensure that login page shown');
+$I->amOnPage('/login');
+$I->see('Password');
+
+$I->wantTo('Ensure that register page shown');
+$I->amOnPage('/register');
+$I->see('First Name');
