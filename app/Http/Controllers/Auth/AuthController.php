3412e5a2b46c1c13b8d1d35971ebb6898b60b2d4
Prasit Gebsaap <prasit.gebsaap@gmail.com>
2016-06-09 00:32:06 +0700

    Fix error during user registration.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
-use App\Models\Access\User;
+use App\Models\Access\User\User;
 use Illuminate\Foundation\Auth\AuthenticatesAndRegistersUsers;
 use Illuminate\Foundation\Auth\ThrottlesLogins;
 use Validator;
--- app/Http/Controllers/User/ProfileController.php
+++ app/Http/Controllers/User/ProfileController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\User;
 use Auth;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\User\UpdateProfileRequest;
-use App\Repositories\Access\User\UserRepositoryContract;
 
 /**
  * Class ProfileController
